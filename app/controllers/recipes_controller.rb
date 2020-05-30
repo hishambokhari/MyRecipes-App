b75ec95505d35435d56413172cd908e037abab08
Hisham Bokhari <hisham.bokhari@cloudasset.com>
2020-05-30 12:50:40 +0500

    Complete: Edit and update recipe feature

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -30,7 +30,8 @@ class RecipesController < ApplicationController
   def update
     @recipe = Recipe.find(params[:id])
     if @recipe.update(recipe_params)
-      #handle this
+      flash[:success] = 'Recipe was updated successfully!'
+      redirect_to recipe_path(@recipe)
     else
       render 'edit'
     end
