c7d34544cecc7a17a1d162951eca6d9aab91c144
Hisham Bokhari <hisham.bokhari@cloudasset.com>
2020-05-31 17:07:05 +0500

    Add: Log in and Log out functionality

--- config/routes.rb
+++ config/routes.rb
@@ -16,8 +16,8 @@ Rails.application.routes.draw do
   
   get '/login', to: 'sessions#new'
   post '/login', to: 'sessions#create'
-  delete '/logout', to: 'sessions#destroy'
-  
+  post '/logout', to: 'sessions#destroy', :as => :logout_path
+
 end
 
 
